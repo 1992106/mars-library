63e9ed9ba6bee4542173e2af3616e04493f3aabb
whj <153104389@qq.com>
2021-05-21 15:10:56 +0800

    fix: vite library

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   rules: {
     'no-empty-source': null,
-    'block-no-empty': true,
     'selector-pseudo-element-no-unknown': [true, {
       'ignorePseudoElements': [
         'v-deep'
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
@@ -7,67 +6,76 @@ import styleImport from 'vite-plugin-style-import'
 const pathResolve = (pathUrl) => path.join(__dirname, pathUrl)
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx({}),
-    styleImport({
-      libs: [
-        {
-          libraryName: 'ant-design-vue',
-          esModule: true,
-          resolveStyle: (name) => `ant-design-vue/es/${name}/style/index`
-        }
-      ]
-    })
-  ],
-  resolve: {
-    alias: {
-      '@src': pathResolve('src'),
-      '@components': pathResolve('src/components'),
-      '@views': pathResolve('src/views'),
-      '@layout': pathResolve('src/layout'),
-      '@config': pathResolve('src/config'),
-      '@utils': pathResolve('src/utils'),
-      '@store': pathResolve('src/store')
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        modifyVars: {},
-        javascriptEnabled: true
-      },
-      scss: {
-        additionalData: '@import "./src/styles/global.scss";'
-      }
-    }
-  },
-  server: {
-    port: 8888
-  },
-  optimizeDeps: {
-    include: [
-      '@ant-design/icons-vue',
-      'ant-design-vue/es/locale/zh_CN',
-      'ant-design-vue/es/locale/en_US',
-      'lodash',
-      'dayjs'
-    ],
-    exclude: []
-  },
-  build: {
-    lib: {
-      entry: '',
-      name: 'mars',
-      formats: ['es', 'cjs', 'iife', 'umd'],
-      fileName: 'mars-library'
-    },
-    terserOptions: {
-      compress: {
-        keep_infinity: true,
-        drop_console: true
+export default ({ mode }) => {
+  let build = {}
+  if (mode === 'lib') {
+    build = {
+      lib: {
+        entry: './packages/index.js',
+        name: 'mars',
+        formats: ['es', 'cjs', 'iife', 'umd'],
+        fileName: 'mars-library'
       }
     }
   }
-})
+  return {
+    plugins: [
+      vue(),
+      vueJsx({}),
+      styleImport({
+        libs: [
+          {
+            libraryName: 'ant-design-vue',
+            esModule: true,
+            resolveStyle: (name) => `ant-design-vue/es/${name}/style/index`
+          }
+        ]
+      })
+    ],
+    resolve: {
+      alias: {
+        '@packages': pathResolve('packages'),
+        '@src': pathResolve('src'),
+        '@components': pathResolve('src/components'),
+        '@views': pathResolve('src/views'),
+        '@layout': pathResolve('src/layout'),
+        '@config': pathResolve('src/config'),
+        '@utils': pathResolve('src/utils'),
+        '@store': pathResolve('src/store')
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          modifyVars: {},
+          javascriptEnabled: true
+        },
+        scss: {
+          additionalData: '@import "./src/styles/global.scss";'
+        }
+      }
+    },
+    server: {
+      port: 8888
+    },
+    optimizeDeps: {
+      include: [
+        '@ant-design/icons-vue',
+        'ant-design-vue/es/locale/zh_CN',
+        'ant-design-vue/es/locale/en_US',
+        'lodash',
+        'dayjs'
+      ],
+      exclude: []
+    },
+    build: {
+      ...build,
+      terserOptions: {
+        compress: {
+          keep_infinity: true,
+          drop_console: true
+        }
+      }
+    }
+  }
+}
