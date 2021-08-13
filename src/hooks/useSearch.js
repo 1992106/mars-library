972e617e9add11b2ba01b86092b0e427802c82d8
whj <153104389@qq.com>
2021-08-13 16:57:52 +0800

    fix: MarsSearch

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -3,8 +3,8 @@ import { isFunction } from 'lodash-es'
 
 export function useSearch(fn) {
   const searchParams = ref({})
-  const handleSearch = ($event = {}) => {
-    searchParams.value = $event
+  const handleSearch = $event => {
+    if ($event) searchParams.value = $event
     if (isFunction(fn)) {
       fn({ ...unref(paramsRef), page: 1 })
     }
