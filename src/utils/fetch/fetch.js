4a99916e5b917975e655ec161f19084e9eeb75cc
王会俊 <huijun.wang@patpat.com>
2021-11-16 20:25:11 +0800

    fix: fetch请求超时

--- src/utils/fetch/fetch.js
+++ src/utils/fetch/fetch.js
@@ -30,7 +30,7 @@ const initRequest = (url, body, method) => {
     method: method,
     headers: initHeaders(method),
     ...(method === 'POST' ? { body: JSON.stringify(body) } : {})
-    // credentials: 'include' // include: cookie既可以同域发送，也可以跨域发送; // 后端Access-Control-Allow-Credentials设置为true时, Access-Control-Allow-Origin不能设置为*，需要设置动态的origin
+    // credentials: 'include' // include: cookie既可以同域发送，也可以跨域发送。（后端Access-Control-Allow-Credentials设置为true时, Access-Control-Allow-Origin不能设置为*，需要设置动态的origin）
   }
   const aplUrl = setting.mock ? url : `${setting.base_url}${url}`
   return new Request(aplUrl, myInit)
@@ -41,11 +41,12 @@ const initRequest = (url, body, method) => {
  * @param url
  * @param body
  * @param method
+ * @param timeout 请求超时时间
  * @returns {Promise<unknown>}
  */
-const myFetch = (url, body, method = 'POST') => {
+const myFetch = (url, body, method = 'POST', timeout = 3000) => {
   const myRequest = initRequest(url, body, method)
-  return new Promise((resolve, reject) => {
+  const fetchPromise = new Promise((resolve, reject) => {
     fetch(myRequest)
       .then(response => {
         // 当接收到一个代表错误的 HTTP 状态码时，从 fetch() 返回的 Promise 不会被标记为 reject， 即使响应的 HTTP 状态码是 404 或 500。
@@ -57,7 +58,7 @@ const myFetch = (url, body, method = 'POST') => {
         // TODO: 为了在fetch的catch中统一处理错误，只能使用以下2种方法 throw 或者 return Promise.reject抛出异常
         // throw new Error(response)
         // return Promise.reject(response)
-        return response
+        return Promise.reject(response)
       })
       .then((res = {}) => {
         // TODO: 本地mock返回的数据格式；真实情况下，需要根据后端返回的数据格式处理
@@ -71,7 +72,8 @@ const myFetch = (url, body, method = 'POST') => {
         }
       })
       .catch(err => {
-        // TODO: 该catch只捕获fetch内部抛出的异常（即第一个then中 throw 或者 return Promise.reject）
+        // promise对象里面同步代码抛出的错误在没有通过promise的catch方法捕获时是会打印报错的（不会阻止promise外面代码的执行），但是不会传递到外面触发其他错误监听函数（比如window.onerror、try-catch等）
+        // TODO: 该catch只捕获fetch内部抛出的异常（即then中 throw 或者 return Promise.reject）
         // TODO: 全局请求错误拦截
         switch (err.status) {
           case 403:
@@ -89,6 +91,14 @@ const myFetch = (url, body, method = 'POST') => {
         reject(err)
       })
   })
+  // 实现请求超时
+  const timeoutPromise = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      reject(new Error('fetch timeout'))
+    }, timeout)
+  })
+  return Promise.race([timeoutPromise, fetchPromise])
 }
 
 export default myFetch
+// https://www.cnblogs.com/wonyun/p/fetch_polyfill_timeout_jsonp_cookie_progress.html
